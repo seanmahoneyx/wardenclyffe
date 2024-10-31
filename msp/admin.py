46b66a34d95947717d85c91befe62effd5de0e1f
seanmahoney621 <35300237+seanmahoney621@users.noreply.github.com>
2024-10-31 16:13:43 -0400

    finished troubleshooting models and migrated them. now time to work on DRF (serializers), sveltekit, and tailwind. also going to use session based authentication

--- msp/admin.py
+++ msp/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import (
+    Item, SalesRep, Employee, Terms, Tax, Account, Customer, Vendor,
+    CustomerLocation, VendorLocation, Contact, CostListHead, CostListLine,
+    PriceListHead, PriceListLine, ContractHead, ContractLine, DropShipHead,
+    DropShipLine, ReleaseHead, ReleaseLine, PurchaseHead, PurchaseLine
+)
 
-# Register your models here.
+# Register models
+admin.site.register(Item)
+admin.site.register(SalesRep)
+admin.site.register(Employee)
+admin.site.register(Terms)
+admin.site.register(Tax)
+admin.site.register(Account)
+admin.site.register(Customer)
+admin.site.register(Vendor)
+admin.site.register(CustomerLocation)
+admin.site.register(VendorLocation)
+admin.site.register(Contact)
+admin.site.register(CostListHead)
+admin.site.register(CostListLine)
+admin.site.register(PriceListHead)
+admin.site.register(PriceListLine)
+admin.site.register(ContractHead)
+admin.site.register(ContractLine)
+admin.site.register(DropShipHead)
+admin.site.register(DropShipLine)
+admin.site.register(ReleaseHead)
+admin.site.register(ReleaseLine)
+admin.site.register(PurchaseHead)
+admin.site.register(PurchaseLine)
--- wardenclyffe/settings.py
+++ wardenclyffe/settings.py
@@ -119,11 +119,10 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',  # Require authentication by default
-    ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.BasicAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
     ],
 }
